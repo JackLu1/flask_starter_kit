9cf93f9cd234edb7d3bc6b43f84d1331c059907d
Jack Lu <jlu9@stuy.edu>
2018-11-14 14:09:08 -0500

    d

--- app.py
+++ app.py
@@ -3,13 +3,23 @@
 # Kxx -- hwname
 # 201x-xx-xx
 
+# stdlib
+import json
+from urllib import request
+
+# pip install
 from flask import Flask, render_template
 
+# custom modules
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "index"
+
+KEY = ""
+URL_STUB = ""
+URL = URL_STUB + KEY
 
 app.debug = True
 app.run()
